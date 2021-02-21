0e3dff79b91d6b27f031b18f7696267c2ece8558
Carlos <carlos_alberto_lira@hotmail.com>
2021-02-20 20:13:15 -0600

    Se realizaron modificaciones en el header para que se renderice o no el componente segun el scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavBarHeader } from "./header/NavBarHeader";
 import { PanelHeader } from "./header/PanelHeader";
-// import { animateScroll as scroll } from "react-scroll";
 
 export const Header = () => {
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentScroll = window.pageYOffset;
+    console.log(currentScroll);
+
+    const isScroll = window.scrollY;
+
+    if (isScroll >= 162) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [visible]);
+
   return (
+    // <header className="sticky-top">
+    //   {visible && <PanelHeader />}
+    //   <NavBarHeader />
+    // </header>
     <header className="sticky-top">
-      <PanelHeader />
+      {visible && <PanelHeader />}
       <NavBarHeader />
     </header>
   );
--- src/components/main/Galeria.js
+++ src/components/main/Galeria.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { CardDeck, Card, Container } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import bruno from "../../assets/bruno.JPG";
 import jordi from "../../assets/jordi.PNG";
 import willi from "../../assets/willi.PNG";
 import mozart from "../../assets/mozart.JPG";
+import placagato3 from "../../assets/placagato3.JPG";
+import toto from "../../assets/toto.JPG";
+import huesos from "../../assets/huesos.JPG";
 import "../../App.css";
 
 export const Galeria = () => {
@@ -12,49 +16,52 @@ export const Galeria = () => {
         <div className="mt-5 mb-5" id="galeria">
           <h2 className="text-center mt-4">Galería</h2>
         </div>
-        <CardDeck>
-          <Card>
-            <Card.Img variant="top" src={willi} />
-            <Card.Body>
-              <Card.Title>Willy</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src={jordi} />
-            <Card.Body>
-              <Card.Title>Jordi</Card.Title>
-              <Card.Text>
-                This card has supporting text below as a natural lead-in to
-                additional content.{" "}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-          <Card>
-            <Card.Img variant="top" src={mozart} />
-            <Card.Body>
-              <Card.Title>Mozart</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer>
-          </Card>
-        </CardDeck>
+        <Row>
+          <Col sm={12} lg={4} className="d-flex justify-content-center ">
+            <Image
+              src={bruno}
+              rounded
+              style={{ width: "200px", height: "200px" }}
+            />
+          </Col>
+          <Col sm={12} lg={4} className="d-flex justify-content-center mt-3">
+            <Image
+              src={willi}
+              rounded
+              style={{ width: "200px", height: "200px" }}
+            />
+          </Col>
+          <Col sm={12} lg={4} className="d-flex justify-content-center mt-3">
+            <Image
+              src={placagato3}
+              rounded
+              style={{ width: "200px", height: "200px" }}
+            />
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col sm={12} lg={4} className="d-flex justify-content-center mt-3">
+            <Image
+              src={huesos}
+              rounded
+              style={{ width: "200px", height: "200px" }}
+            />
+          </Col>
+          <Col sm={12} lg={4} className="d-flex justify-content-center mt-3">
+            <Image
+              src={jordi}
+              rounded
+              style={{ width: "200px", height: "200px" }}
+            />
+          </Col>
+          <Col sm={12} lg={4} className="d-flex justify-content-center mt-3">
+            <Image
+              src={mozart}
+              rounded
+              style={{ width: "200px", height: "200px" }}
+            />
+          </Col>
+        </Row>
       </Container>
     </>
   );
