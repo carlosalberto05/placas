cf4b7dbaff0f317e0cacc335436de97fbdf2c8fc
Carlos <carlos_alberto_lira@hotmail.com>
2021-02-23 09:43:26 -0600

    Se creo el componente del carousel

--- src/components/main/Products.js
+++ src/components/main/Products.js
@@ -1,30 +1,37 @@
 import React from "react";
 import { Container, Jumbotron, Carousel } from "react-bootstrap";
-import willi from "../../assets/willi.PNG";
+import carousel from "../../assets/carousel.jpg";
+import carousel2 from "../../assets/carousel2.jpg";
+import carousel3 from "../../assets/carousel3.PNG";
 import "../../App.css";
 
 export const Products = () => {
   return (
     <div id="products" className="mt-5">
       <h2 className="text-center">Productos</h2>
-      <Jumbotron className="jb-productos">
+      <Jumbotron className="jm-carousel">
         <Container>
-          <Carousel>
-            <Carousel.Item interval={1000}>
-              <img className="d-block w-100" src={willi} alt="First slide" />
-              <Carousel.Caption>
+          <Carousel className="stilos-carousel">
+            <Carousel.Item>
+              <img
+                className="d-block w-100  "
+                src={carousel}
+                alt="First slide"
+              />
+              <Carousel.Caption className="stilos-carousel-caption">
                 <h3>First slide label</h3>
                 <p>
                   Nulla vitae elit libero, a pharetra augue mollis interdum.
                 </p>
               </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={500}>
+            <Carousel.Item>
               <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
+                src={carousel2}
                 alt="Second slide"
               />
+
               <Carousel.Caption>
                 <h3>Second slide label</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -33,9 +40,10 @@ export const Products = () => {
             <Carousel.Item>
               <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
+                src={carousel3}
                 alt="Third slide"
               />
+
               <Carousel.Caption>
                 <h3>Third slide label</h3>
                 <p>
