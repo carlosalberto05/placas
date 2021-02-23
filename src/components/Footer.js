a5b0f29df6670d7cf73bc200906c74aebbcd9266
Carlos <carlos_alberto_lira@hotmail.com>
2021-02-23 10:30:10 -0600

    Se agregó contendio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
 export const Footer = () => {
   return (
-    <div>
-      <h3>Footer</h3>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <img
+            src={logo}
+            alt="logo"
+            style={{ width: "5rem", height: "auto" }}
+          />
+          <p>
+            This prodigiously grew tortoise charact stupidly pernicious jeepers
+            along while accordingly under useful much salacious walking fars ...
+          </p>
+        </Col>
+        <Col>
+          <h3>Información de Contacto</h3>
+          <p>Cunduacán, Tabasco</p>
+          <p>Cel: 9934366178</p>
+          <p>carlos_alberto-lira@outlook.com</p>
+        </Col>
+        <Col>
+          <h3>Siguenos en </h3>
+          <p>Instagram</p>
+          <p>Facebook</p>
+          <p>Whatsapp</p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
