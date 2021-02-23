ce688779ea9098c5f3d0a0077a527b815a92bb79
Carlos <carlos_alberto_lira@hotmail.com>
2021-02-22 19:59:52 -0600

    Se agrego un carrousel

--- src/components/main/Products.js
+++ src/components/main/Products.js
@@ -1,13 +1,50 @@
 import React from "react";
-import { Container, Jumbotron } from "react-bootstrap";
+import { Container, Jumbotron, Carousel } from "react-bootstrap";
+import willi from "../../assets/willi.PNG";
+import "../../App.css";
 
 export const Products = () => {
   return (
-    <div id="products">
-      <h2>Products</h2>
-      <Jumbotron>
+    <div id="products" className="mt-5">
+      <h2 className="text-center">Productos</h2>
+      <Jumbotron className="jb-productos">
         <Container>
-          <h4>Desde los productos</h4>
+          <Carousel>
+            <Carousel.Item interval={1000}>
+              <img className="d-block w-100" src={willi} alt="First slide" />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={500}>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Second slide&bg=282c34"
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </Container>
       </Jumbotron>
     </div>
